2d7cbcf7c5102c46152edd46add385ec0546266d
posadari <57573681+posadari@users.noreply.github.com>
2021-10-31 02:34:53 -0500

    polish UI and add firebase hosting

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,6 +5,11 @@ import Button from '@mui/material/Button';
 import { setLocation } from '../app/matchSlice'
 import { useDispatch } from 'react-redux'
 import {Link} from "react-router-dom";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import img1 from '../images/nature.svg'
 
 const Home = () => {
     const [location, setStateLocation] = useState('');
@@ -22,18 +27,37 @@ const Home = () => {
 
   return (
       <div>
-          <h1>Find a National Park</h1>
+
+    <Card sx={{backgroundColor: '#F9E9DA', maxWidth: 400, position: 'absolute', left: '50%', top: '45%',
+        transform: 'translate(-50%, -50%)', height: 600 }}>
+            
+      <CardContent>
+        <Typography style={{textAlign: 'center', marginTop:'0.8em'}} variant="h3" component="div">
+          Find National Parks to Visit!
+        </Typography>
         <Box
-        component="form"
-        sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
+            component="form"
+            sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
         >
-            <TextField id="standard-basic" label="Select a State" variant="standard" value={location} onChange={onLocationChange} required /> 
-            <Link to="/results"><Button variant="dark" onClick={handleClick}>Submit</Button></Link>
+            <img style={{position: 'absolute', left: '50%', top: '40%', transform: 'translate(-50%, -50%)'}}src={img1}/>
+            <TextField style={{position: 'absolute', left: '50%', top: '60%', transform: 'translate(-50%, -50%)'}} id="standard-basic" label="Enter a state's abbrev." 
+                       variant="standard" value={location} onChange={onLocationChange} required /> 
         </Box>
+      </CardContent>
+      <CardActions>
+          <Link to="/results"><Button style={{position: 'absolute', left: '50%', top: '73%', transform: 'translate(-50%, -50%)'}} variant="contained" 
+                onClick={handleClick}>Submit</Button></Link>
+      </CardActions>
+      
+    </Card>
+
+        
+        
+       
     </div>
   );
 }
